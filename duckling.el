95d944dc09497aa192e60c3093e482cee55d8a7b
Abhinav Tushar <lepisma@fastmail.com>
2018-09-11 02:47:21 +0530

    el

---
+++ duckling.el
@@ -0,0 +1,39 @@
+;;; duckling.el --- Minimal wrapper for duckling -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2018 Abhinav Tushar
+
+;; Author: Abhinav Tushar <lepisma@fastmail.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25"))
+;; URL: https://github.com/lepisma/duckling.el
+
+;;; Commentary:
+
+;; Minimal wrapper for duckling
+;; This file is not a part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;;###autoload
+(defun duckling-parse (text &optional lang tz ref-time)
+  "Parse TEXT using duckling. Pass LANG, TZ (timezone) and REF-TIME as args."
+  (user-error "Not implemented"))
+
+(provide 'duckling)
+
+;;; duckling.el ends here
