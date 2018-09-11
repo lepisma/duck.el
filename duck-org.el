3fbf722fbecc5d26c65dc7c5c9ca16dd5b26cb53
Abhinav Tushar <lepisma@fastmail.com>
2018-09-12 00:53:56 +0530

    duck-org file

---
+++ duck-org.el
@@ -0,0 +1,34 @@
+;;; duck-org.el --- Org mode stuff for duck.el -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2018 Abhinav Tushar
+
+;; Author: Abhinav Tushar <lepisma@fastmail.com>
+
+;;; Commentary:
+
+;; Org mode stuff for duck.el
+;; This file is not a part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'duck-time)
+
+
+(provide 'duck-org)
+
+;;; duck-org.el ends here
